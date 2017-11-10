dda82f38cf8b420f93ea76ebf287b87da6b816b1
Jack <jacktbader5@gmail.com>
2017-11-10 14:31:39 -0700

    test

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+//
 
 const path = require('path');
 
